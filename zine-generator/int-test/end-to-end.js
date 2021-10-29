6cea1defbbdf71d6a350c93d9ed61e6e38e61dc4
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-29 16:48:25 -0400

    Expect 404

--- zine-generator/int-test/end-to-end.js
+++ zine-generator/int-test/end-to-end.js
@@ -22,7 +22,7 @@ describe('End-to-end integration tests', () => {
 			})
 			.end((error, res) => {
 				expect(error).to.equal(null);
-				expect(res.status).to.equal(200);
+				expect(res.status).to.equal(404);
 			});
 		done();
 	}).timeout(30_000);
