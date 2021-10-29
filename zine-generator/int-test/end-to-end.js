de8729ef394f8abb935d25a54b690fc3622f4395
superflyxxi <40075662+superflyxxi@users.noreply.github.com>
2021-10-29 17:46:39 -0400

    feat: zine generator basic (#2)
    * Don't check coverage
    
    * xo-fix
    
    * Ignore prettier
    
    * Fixed error handler index
    
    * Sonar scan sources
    
    * Untar the coverage
    
    * Include template
    
    * Dynamic sonar-project.properties
    
    * Don't do max depth
    
    * Fixed env var
    
    * Expect 404

--- zine-generator/int-test/end-to-end.js
+++ zine-generator/int-test/end-to-end.js
@@ -18,11 +18,11 @@ describe('End-to-end integration tests', () => {
 			.send({
 				generator: true,
 				from: false,
-				to: false
+				to: false,
 			})
 			.end((error, res) => {
 				expect(error).to.equal(null);
-				expect(res.status).to.equal(200);
+				expect(res.status).to.equal(404);
 			});
 		done();
 	}).timeout(30_000);
