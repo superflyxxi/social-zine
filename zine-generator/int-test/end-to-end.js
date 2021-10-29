81e432292d413497fc0e95ae8112f4d72881af1a
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-29 15:51:42 -0400

    zine-generator

---
+++ zine-generator/int-test/end-to-end.js
@@ -0,0 +1,29 @@
+import process from 'node:process';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import chaiAlmost from 'chai-almost';
+
+const {expect} = chai;
+
+const BASE_URL = process.env.BASE_URL ?? 'http://localhost:3000';
+
+chai.use(chaiHttp);
+chai.use(chaiAlmost(0.1));
+
+describe('End-to-end integration tests', () => {
+	it('Do it', (done) => {
+		chai
+			.request(BASE_URL)
+			.post('/v1/zine')
+			.send({
+				generator: true,
+				from: false,
+				to: false
+			})
+			.end((error, res) => {
+				expect(error).to.equal(null);
+				expect(res.status).to.equal(200);
+			});
+		done();
+	}).timeout(30_000);
+});
