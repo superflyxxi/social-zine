aa33b798bc66728e1d7fa5c580fa90ee44abd0e8
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-30 10:32:28 -0400

    Test no rankRules

--- zine-generator/test/services/rank-score-test.js
+++ zine-generator/test/services/rank-score-test.js
@@ -1,16 +1,18 @@
-import service from '../../src/services/rank-score.js';
-import { strict as assert } from 'assert';
+import {strict as assert} from 'node:assert';
+import rank from '../../src/services/rank-score.js';
 
 describe('Rank Score Tests', () => {
-	it('Rank empty items', (done) => {
-		const res = service.rank({}, [], []);
+	it('Rank empty items', async () => {
+		const res = await rank({}, [], []);
 		assert.deepEqual(res, []);
-		done();
 	});
 
-	it('Rank no rules', (done) => {
-		assert.fail('Missing tests');
-		done();
+	it('Rank no rules', async () => {
+		const res = await rank({}, ['attr1'], [{attr1: 1}, {attr1: 2}]);
+		assert.deepEqual(res, [
+			{score: 0, scoreBreakdown: {attr1: 0}},
+			{score: 0, scoreBreakdown: {attr1: 0}},
+		]);
 	});
 
 	it('Rank empty ranklist', (done) => {
