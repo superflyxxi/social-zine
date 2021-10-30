4fe7afa2f714e12108c7aee9eeda07ec9567bd3a
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-30 13:42:24 -0400

    Use chai

--- common/test/error-handler-test.js
+++ common/test/error-handler-test.js
@@ -1,17 +1,40 @@
 import {strict as assert} from 'node:assert';
 import library from '../src/index.js';
+import chai from 'chai';
+const {expect} = chai;
 
 describe('Error-handler', () => {
 	it('All attributes populated', () => {
 		let nextCalled = false;
-		let sendCalled =false;
-		const res = {
-			status: (input) => {
-				return { send: (input) => { sendCalled = true;} };
-			}
-		};
-		library.errorHandler({type: 'MyType', title: 'My Title', status: 400, detail: 'My Detail'},{headersSent: true}, res, () => { nextCalled=true;});
-		assert.deepEqual(sendCalled, true, 'Send Called');
-		assert.deepEqual(nextCalled, false, 'Next Called');
+		let message;
+	const res = {
+		status: (input) => {
+			return { send: (input) => { message = input;} };
+		},
+	};
+		library.errorHandler({type: 'MyType', title: 'My Title', status: 400, detail: 'My Detail'}, undefined, res, () => { nextCalled=true;});
+		expect(nextCalled).to.deep.equal(false, 'Next Called');
+		expect(message).to.deep.include({
+			type: 'MyType',
+			title: 'My Title',
+			status: 400,
+			detail: 'My Detail',
+		});
+	});
+
+	it('No attributes populated', () => {
+		expect.fail('Missing test');
+	});
+
+	it('Minimum attributes populated', () => {
+		expect.fail('Missing test');
+	});
+
+	it('No attributes populated', () => {
+		expect.fail('Missing test');
+	});
+
+	it('No attributes populated', () => {
+		expect.fail('Missing test');
 	});
 });
