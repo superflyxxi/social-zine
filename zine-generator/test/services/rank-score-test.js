aa1705c4a555f8ce561e053b40204e5f3d1e364d
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-30 09:48:15 -0400

    Basic rank-score test

---
+++ zine-generator/test/services/rank-score-test.js
@@ -0,0 +1,9 @@
+import service from '../../src/services/rank-score.js';
+import { strict as assert } from 'assert';
+
+describe('Rank Score Tests', () => {
+	it('Rank empty', (done) => {
+		assert.fail('Missing tests');
+		done();
+	});
+});
