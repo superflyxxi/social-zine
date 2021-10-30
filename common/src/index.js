85b5d5efd779789bcc95a1bbd75263699840cd24
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-30 13:05:59 -0400

    initial common

---
+++ common/src/index.js
@@ -0,0 +1,32 @@
+import process from 'node:process';
+import {v4 as uuidv4} from 'uuid';
+
+function errorHandler(error, req, res, next) {
+	console.log('error encountered', error);
+	if (res.headersSent) {
+		return next(error);
+	}
+
+	const message = {
+		type: error.type ?? '/errors/SYSTEM_ERROR',
+		title: error.title ?? 'System Error',
+		status: error.status ?? 500,
+		detail: error.detail ?? error.message ?? 'An unknown system error has occurred.',
+		instance: uuidv4(),
+		stack: process.env.NODE_ENV === 'production' ? undefined : error.stack,
+	};
+	res.status(message.status).send(message);
+}
+
+class RootError extends Error {
+	constructor(type, title, status, detail) {
+		super(detail);
+		this.name = 'RootError';
+		this.type = type;
+		this.title = title;
+		this.status = status;
+		this.detail = detail;
+	}
+}
+
+export default {errorHandler, RootError}
