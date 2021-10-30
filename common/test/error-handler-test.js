c3bf551c298cc5b694891ab83588c60fe4df2060
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-30 13:36:56 -0400

    include chai

--- common/test/error-handler-test.js
+++ common/test/error-handler-test.js
@@ -1,8 +1,17 @@
 import {strict as assert} from 'node:assert';
 import library from '../src/index.js';
 
-describe('End-to-end tests', () => {
-	it('Template', () => {
-		assert.fail('Missing test');
+describe('Error-handler', () => {
+	it('All attributes populated', () => {
+		let nextCalled = false;
+		let sendCalled =false;
+		const res = {
+			status: (input) => {
+				return { send: (input) => { sendCalled = true;} };
+			}
+		};
+		library.errorHandler({type: 'MyType', title: 'My Title', status: 400, detail: 'My Detail'},{headersSent: true}, res, () => { nextCalled=true;});
+		assert.deepEqual(sendCalled, true, 'Send Called');
+		assert.deepEqual(nextCalled, false, 'Next Called');
 	});
 });
