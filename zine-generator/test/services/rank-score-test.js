c90f7c12bce2066bff453fc666094e5100207086
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-31 16:29:54 -0400

    test empty rank

--- zine-generator/test/services/rank-score-test.js
+++ zine-generator/test/services/rank-score-test.js
@@ -1,13 +1,13 @@
 import {strict as assert} from 'node:assert';
 import rank from '../../src/services/rank-score.js';
 
-describe('Rank Score Tests', function () {
-	it('Rank empty items', async function () {
+describe('Rank Score Tests', function() {
+	it('Rank empty items', async function() {
 		const res = await rank({}, [], []);
 		assert.deepEqual(res, []);
 	});
 
-	it('Rank no rules', async function () {
+	it('Rank no rules', async function() {
 		const res = await rank({}, ['attr1'], [{attr1: 1}, {attr1: 2}]);
 		assert.deepEqual(res, [
 			{score: 0, scoreBreakdown: {attr1: 0}},
@@ -15,7 +15,13 @@ describe('Rank Score Tests', function () {
 		]);
 	});
 
-	it('Rank empty ranklist');
+	it('Rank empty ranklist', async function() {
+		const res = await rank({}, [], [{attr1: 1}, {attr1: 2}]);
+		assert.deepEqual(res, [
+			{score: 0, scoreBreakdown: {}},
+			{score: 0, scoreBreakdown: {}},
+		]);
+	});
 
 	it('Rank single');
 
