b4700948884991563da5f75750e5af81442a6a67
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-31 13:49:55 -0400

    Use common

--- zine-generator/src/controllers/generator.js
+++ zine-generator/src/controllers/generator.js
@@ -1,9 +1,9 @@
 import {rankRules} from '../config/index.js';
-import validation from '../helpers/validation.js';
+import {validate} from '@superflyxxi/common';
 import rank from '../services/rank-score.js';
 
 export default async function generate(req, res) {
-	validation(req.body, {
+	validate(req.body, {
 		startDate: {presence: true, type: 'date'},
 		endDate: {presence: true, type: 'date'},
 	});
