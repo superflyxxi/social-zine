3bf05cec4ccb5570f83d8c2c8e05cdc363ad9aab
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-31 15:40:35 -0400

    Test boolean

--- common/test/validation-test.js
+++ common/test/validation-test.js
@@ -3,75 +3,116 @@ import {validate, ValidationError} from '../src/index.js';
 
 const {expect} = chai;
 
-const everything = {s: 'a', b: true, i: 1, d: 1.1, a: [1, 2, 3], o: {name:'Object'}};
+const everything = {s: 'a', b: true, i: 1, d: 1.1, a: [1, 2, 3], o: {name: 'Object'}};
 
-describe('Basic Validation', () => {
-	it('Empty rules', () => {
+describe('Basic Validation', function () {
+	it('Empty rules', function () {
 		validate(everything, {});
 	});
 
-	it('More attributes than expected', () => {
+	it('More attributes than expected', function () {
 		validate(everything, {
 			z: {presence: false, type: 'string'},
 		});
 	});
 
-	it('Required attributes', () => {
+	it('Required attributes', function () {
 		validate(everything, {
 			s: {presence: true},
 		});
 	});
 
-
-	it('Bool', () => {
-		validate(everything, {
-			b: {presence: true, type: 'boolean'},
+	describe('Sting Validations', function () {
+		it('String', function () {
+			validate(everything, {
+				s: {presence: false, type: 'string'},
+			});
+		});
+		it('Bool', function () {
+			expect(function () {
+				validate(everything, {
+					b: {presence: false, type: 'string'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Integer', function () {
+			expect(function () {
+				validate(everything, {
+					i: {presence: false, type: 'string'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Decimal', function () {
+			expect(function () {
+				validate(everything, {
+					d: {presence: false, type: 'string'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Array', function () {
+			expect(function () {
+				validate(everything, {
+					a: {presence: false, type: 'string'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Object', function () {
+			expect(function () {
+				validate(everything, {
+					o: {presence: false, type: 'string'},
+				});
+			}).to.throw(ValidationError);
 		});
 	});
 
-	it('Integer', () => {
-		expect.fail('Missing test');
-	});
-
-	it('Decimal', () => {
-		expect.fail('Missing test');
-	});
-
-	it('Array', () => {
-		expect.fail('Missing test');
-	});
-});
-
-
-describe('Sting Validations', () => {
-	it('String', () => {
-		validate(everything, {
-			s: {presence: false, type: 'string'},
+	describe('Boolean Validations', function () {
+		it('String', function () {
+			expect(function () {
+				validate(everything, {
+					s: {presence: false, type: 'bool'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Bool', function () {
+			expect(function () {
+				validate(everything, {
+					b: {presence: false, type: 'bool'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Integer', function () {
+			expect(function () {
+				validate(everything, {
+					i: {presence: false, type: 'bool'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Decimal', function () {
+			expect(function () {
+				validate(everything, {
+					d: {presence: false, type: 'bool'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Array', function () {
+			expect(function () {
+				validate(everything, {
+					a: {presence: false, type: 'bool'},
+				});
+			}).to.throw(ValidationError);
+		});
+		it('Object', function () {
+			expect(function () {
+				validate(everything, {
+					o: {presence: false, type: 'bool'},
+				});
+			}).to.throw(ValidationError);
 		});
 	});
-	it('Bool', () => {
-		expect(() => validate(everything, {
-			b: {presence: false, type: 'string'},
-		})).to.throw(ValidationError);
-	});
-	it('Integer', () => {
-		expect(() => validate(everything, {
-			i: {presence: false, type: 'string'},
-		})).to.throw(ValidationError);
-	});
-	it('Decimal', () => {
-		expect(() => validate(everything, {
-			d: {presence: false, type: 'string'},
-		})).to.throw(ValidationError);
-	});
-	it('Array', () => {
-		expect(() => validate(everything, {
-			a: {presence: false, type: 'string'},
-		})).to.throw(ValidationError);
-	});
-	it('Object', () => {
-		expect(() => validate(everything, {
-			o: {presence: false, type: 'string'},
-		})).to.throw(ValidationError);
-	});
+
+	it('Integer');
+
+	it('Decimal');
+
+	it('Array');
 });
