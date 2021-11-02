08283b98a85889a128c43ea5a19fc6cd547c0c40
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-01 22:10:21 -0400

    make it simpler

--- template/src/index.js
+++ template/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import {RouteNotFoundError, errorHandler} from '@superflyxxi/common';
-import apiDocsRouter from './routers/api-docs/index.js';
+import apiDocsRouter from './routers/api-docs.js';
 import {server} from './config/index.js';
 
 const app = express();
