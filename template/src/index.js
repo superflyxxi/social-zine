44664d9ca01d376b6c8c328e0aa327fd64e258f0
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-02 18:58:05 -0400

    Server print

--- template/src/index.js
+++ template/src/index.js
@@ -2,6 +2,7 @@ import {serverConfig, createServer} from '@superflyxxi/common';
 
 const app = createServer('Template', serverConfig.version, function (server) {
 	// Add your api to server here
+	console.log(server);
 });
 app.listen(serverConfig.port, function () {
 	console.log('Started serviceNameHere (', serverConfig.version, ') listening on', serverConfig.port);
