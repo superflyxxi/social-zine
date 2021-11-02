09af69f0329144257fdc8cfbfaf2aad2fa6f4f40
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-02 14:17:43 -0400

    Added documentation for error

--- common/src/index.js
+++ common/src/index.js
@@ -5,6 +5,36 @@ import swaggerJsdoc from 'swagger-jsdoc';
 import validatejs from 'validate.js';
 import {v4 as uuidv4} from 'uuid';
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Error:
+ *       type: object
+ *       properties:
+ *         type:
+ *           type: string
+ *           format: uri
+ *           description: The type of error that has occurred.
+ *           example: '/errors/SYSTEM_ERROR'
+ *         title:
+ *           type: string
+ *           description: A human readable title for the error.
+ *           example: System Error
+ *         status:
+ *           type: integer
+ *           description: The HTTP response status of this error.
+ *           example: 500
+ *         detail:
+ *           type: string
+ *           description: Some details about the error.
+ *           example: An unknown system error has occurred.
+ *         instance:
+ *           type: string
+ *           format: uuid
+ *           description: A unique identifier of this instance of the error.
+ *           example: 2c046e7d-8d71-4f4e-9d79-aef50777a9b3
+ */
 export function errorHandler(error, req, res, next) {
 	console.log('error encountered', error);
 	if (res.headersSent) {
@@ -79,7 +109,7 @@ export function getApiDocsRouter(title, version) {
 			openapi: '3.0.0',
 			info: {title, version},
 		},
-		apis: ['./src/routers/**/*.js'],
+		apis: ['./src/routers/**/*.js', '../common/src/index.js'],
 	});
 
 	apiDocs.get('/json', (req, res) => res.send(openapispec));
