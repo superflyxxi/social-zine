a1fb74402c57e45b66be119eb37b1f1d6fcc6d54
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-02 18:21:59 -0400

    Fixed tests

--- zine-generator/src/index.js
+++ zine-generator/src/index.js
@@ -1,7 +1,7 @@
-import {startServer} from '@superflyxxi/common';
+import {createServer} from '@superflyxxi/common';
 import {server} from './config/index.js';
 
-const app = startServer('Zines', server.version);
+const app = createServer('Zines', server.version);
 app.listen(server.port, () => {
 	console.log('Started Zines (', server.version, ') listening on', server.port);
 });
