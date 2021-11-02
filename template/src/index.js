4655ba12bc8886209dc11bdb8e26361ee726ec1f
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-02 18:56:28 -0400

    Reverted back to template

--- template/src/index.js
+++ template/src/index.js
@@ -1,9 +1,9 @@
 import {serverConfig, createServer} from '@superflyxxi/common';
 
-const app = createServer('Template', serverConfig.version, function(server) {
-// add your api to server here
- });
- 	app.listen(serverConfig.port, function() {
- 			console.log('Started serviceNameHere (', serverConfig.version, ') listening on', serverConfig.port);
- 				});
- 				export default app;
+const app = createServer('Template', serverConfig.version, function (server) {
+	// Add your api to server here
+});
+app.listen(serverConfig.port, function () {
+	console.log('Started serviceNameHere (', serverConfig.version, ') listening on', serverConfig.port);
+});
+export default app;
