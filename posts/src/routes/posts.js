0665443a1f3444619540c8a33dac9bd2509dc351
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-03 16:32:35 -0400

    Posts?

---
+++ posts/src/routes/posts.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import {getPosts} from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', asyncHandler(controller.getPosts));
+export default router;
