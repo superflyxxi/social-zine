51695d0497a9c4eab065d64fcaf5c70fdbf01f53
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-03 16:58:57 -0400

    Comment about mongo URI

--- posts/src/db/index.js
+++ posts/src/db/index.js
@@ -1,6 +1,7 @@
 import {MongoClient} from 'mongodb';
 import process from 'node:process';
 
+// URI like mongodb://user:password@mongo:27017/
 const client = new MongoClient(process.env.MONGODB_URI);
 await client.connect();
 await client.db("social-zine");
