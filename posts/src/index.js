6c11685746f0c0028eb7948d769b0c0c4a525933
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-03 16:57:27 -0400

    Handle db

--- posts/src/index.js
+++ posts/src/index.js
@@ -1,7 +1,14 @@
 import {serverConfig, createServer} from '@superflyxxi/common';
+import dbClient from './db/index.js';
 
 const app = createServer('Posts');
 app.listen(serverConfig.port, function () {
 	console.log('Started Posts (', serverConfig.version, ') listening on', serverConfig.port);
 });
+process.on('SIGTERM', function (){
+  server.close(function() {
+	dbClient.close();
+  })
+})
+
 export default app;
