4a0bc1eda543420c524fc082e547145a801e3736
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-07 12:44:21 -0500

    Removed duplicate get invalid

--- posts/test/posts-test.js
+++ posts/test/posts-test.js
@@ -23,24 +23,6 @@ describe('Post tests', async () => {
 			});
 	});
 
-	it('Get non-existent', function(done) {
-		chai
-			.request(app)
-			.get('/v1/posts/6188021ac86ae14801525def')
-			.end((error, res) => {
-				expect(res).to.have.status(404);
-				// eslint-disable-next-line no-unused-expressions
-				expect(res).to.be.json;
-				expect(res.body).to.deep.include({
-					type: '/errors/NOT_FOUND',
-					title: 'Not Found',
-					status: res.status,
-					detail: '6188021ac86ae14801525def',
-				});
-				done();
-			});
-	});
-
 	it('One: create, get, list, delete', function (done) {
 		const input = {
 			date: new Date('2021-11-06T17:19:00'),
@@ -104,16 +86,27 @@ describe('Post tests', async () => {
 										console.log('body', res.body);
 										// eslint-disable-next-line no-unused-expressions
 										expect(res.body).to.deep.equal({});
-										done();
+										chai
+											.request(app)
+											.get('/v1/posts/' + input._id)
+											.end((error, res) => {
+												expect(res).to.have.status(404);
+												// eslint-disable-next-line no-unused-expressions
+												expect(res).to.be.json;
+												expect(res.body).to.deep.include({
+													type: '/errors/NOT_FOUND',
+													title: 'Not Found',
+													status: res.status,
+													detail: input._id,
+												});
+												done();
+											});
 									});
 							});
 					});
 			});
 	});
 
-	it('Delete individual');
-	
 	it('Get list of many');
-	it('Create second');
 
 });
