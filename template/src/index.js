d43b9148f5c2647234f5483883640797f4f31d90
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-08 11:32:46 -0500

    Await db connection

--- template/src/index.js
+++ template/src/index.js
@@ -1,4 +1,7 @@
 import {serverConfig, createServer} from '@superflyxxi/common';
+import {connect as connectDatabase} from './db/index.js';
+
+await connectDatabase();
 
 const app = createServer('Template', serverConfig.version, function (server) {
 	// Add your api to server here
