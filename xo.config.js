8333ec2834fa4c37d8c007bc34a6d16104cdc2fd
SuperFlyXXI <superflyxxi@yahoo.com>
2026-09-05 15:55:32 -0400

    ci(xo): ignore unicorn/no-top-level-side-effects

--- xo.config.js
+++ xo.config.js
@@ -1,27 +1,28 @@
 module.exports = {
-	ignores: [],
-	prettier: true,
-	rules: {
-		'unicorn/name-replacements': [
-			'error',
-			{
-				replacements: {
-					res: false,
-					req: false,
-					i: false,
-					dir: false,
-					docs: false
-				}
-			}
-		],
-		'new-cap': [
-			'error',
-			{
-				capIsNewExceptions: ['Router']
-			}
-		],
-		'guard-for-in': 'off',
-		'max-depth': ['error', 5]
-	}
+  ignores: [],
+  prettier: true,
+  rules: {
+    'unicorn/name-replacements': [
+      'error',
+      {
+        replacements: {
+          res: false,
+          req: false,
+          i: false,
+          dir: false,
+          docs: false
+        }
+      }
+    ],
+    'new-cap': [
+      'error',
+      {
+        capIsNewExceptions: ['Router']
+      }
+    ],
+    'guard-for-in': 'off',
+    'max-depth': ['error', 5],
+    'unicorn/no-top-level-side-effects': 'off',
+  }
 };
 
