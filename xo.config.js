63a749b3c37df9577a6a3e6672231400873320cc
SuperFlyXXI <superflyxxi@yahoo.com>
2026-09-05 15:32:56 -0400

    ci(xo): moved config to cjs file and supported xo4

---
+++ xo.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	ignores: [],
+	prettier: true,
+	rules: {
+		'unicorn/name-replacements': [
+			'error',
+			{
+				replacements: {
+					res: false,
+					req: false,
+					i: false,
+					dir: false,
+					docs: false
+				}
+			}
+		],
+		'new-cap': [
+			'error',
+			{
+				capIsNewExceptions: ['Router']
+			}
+		],
+		'guard-for-in': 'off',
+		'max-depth': ['error', 5]
+	}
+};
+
